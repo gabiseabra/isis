f4263bf4394509305ae2158efdcf3635b0c90bbb
Gabriela Seabra <hey@gabiseabra.dev>
2026-06-23 04:42:29 -0300

    fix: safeql in ci??

--- api/safeql.config.ts
+++ api/safeql.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "@ts-safeql/eslint-plugin";
 
 export default defineConfig({
   connections: {
-    databaseUrl: process.env.DATABASE_URL,
-    migrationsDir: "./src/db/schema",
+    connectionUrl: process.env.DATABASE_URL,
+    migrationsDir: process.env.DATABASE_URL ? undefined : "./src/db/schema",
     targets: [
       { tag: "sql", transform: "{type}" },
       { tag: "sqlOne", transform: "{type}" },
